55a031a45df2bc3c68e79103ef3303d8579c8339
Touch Sungkawichai <talay@10ninox.com>
2020-07-13 13:00:45 +0700

    refinement

--- src/router/auto-food.js
+++ src/router/auto-food.js
@@ -2,41 +2,52 @@ const { Subscription } = require('../model')
 const schedule = require('node-schedule')
 const { menuWriter, getDate } = require('./food')
 
-const Subcribe = async (ctx) => {
-  let id = ctx.message.chat.id.toString()
+const query = async () => {
   let res = await Subscription.findOne({ name: 'AutoFood' })
-
   if (!res) res = { userids: [] }
-  let find = res.userids.filter((obj) => {
-    return obj.id === id
-  })
-  if (find == null || find.length == 0)
-    res.userids = [...res.userids, { id, last: null }]
-  await Subscription.updateOne(
+  return res
+}
+
+const update = async (userids) => {
+  return await Subscription.updateOne(
     { name: 'AutoFood' },
-    { userids: res.userids },
+    { userids },
     { upsert: true }
   )
 }
 
+const Subcribe = async (ctx) => {
+  let id = ctx.message.chat.id.toString()
+  let res = await query()
+
+  let find = res.userids.filter((obj) => {
+    return obj.id === id
+  })
+
+  if (find == null || find.length == 0)
+    res.userids = [...res.userids, { id, last: null }]
+
+  await update(res.userids)
+}
+
 const SendMenu = async (bot) => {
-  let res = await Subscription.findOne({ name: 'AutoFood' })
-  if (!res) res = { userids: [] }
+  let res = await query()
+
   for (ind in res.userids) {
     let user = res.userids[ind]
     let date = getDate()
     let msg = await menuWriter(date, ['Breakfast', 'Lunch', 'Dinner'])
+
     if (user.last != null) await bot.telegram.deleteMessage(user.id, user.last)
+
     let sent = await bot.telegram.sendMessage(user.id, msg, {
       parse_mode: 'Markdown',
     })
+
     res.userids[ind] = { id: user.id, last: sent.message_id }
   }
-  await Subscription.updateOne(
-    { name: 'AutoFood' },
-    { userids: res.userids },
-    { upsert: true }
-  )
+
+  await update(res.userids)
 }
 
 const botSendMenu = (bot) => () => SendMenu(bot)
