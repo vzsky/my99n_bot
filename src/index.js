325e6f85b29b14277824ef61d696b56980baf605
Touch Sungkawichai <talay@10ninox.com>
2021-02-22 13:45:20 +0700

    add auto-binance

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const useReminder = require('./router/reminder').main
 const useFood = require('./router/food').main
 const useAutoFood = require('./router/auto-food').main
 const useBinance = require('./router/binance').main
+const useAutoBinance = require('./router/auto-binance').main
 
 const bot = new Telegraf(process.env.TOKEN)
 
@@ -22,5 +23,6 @@ useReminder(bot)
 useFood(bot)
 useAutoFood(bot)
 useBinance(bot)
+useAutoBinance(bot)
 
 bot.launch()
