a5f77b0153914b73050a7e0341892664a20d67ef
Touch Sungkawichai <talay@10ninox.com>
2021-04-04 13:31:20 +0700

    add binance alert

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ require('dotenv').config()
 
 const useMain = require('./router/main').main
 const useReminder = require('./router/reminder').main
-const useFood = require('./router/food').main
-const useAutoFood = require('./router/auto-food').main
+// const useFood = require('./router/food').main
+// const useAutoFood = require('./router/auto-food').main
 const useBinance = require('./router/binance').main
-const useAutoBinance = require('./router/auto-binance').main
+// const useAutoBinance = require('./router/auto-binance').main
 
 const bot = new Telegraf(process.env.TOKEN)
 
@@ -23,6 +23,6 @@ useReminder(bot)
 // useFood(bot)
 // useAutoFood(bot)
 useBinance(bot)
-useAutoBinance(bot)
+// useAutoBinance(bot)
 
 bot.launch()
--- src/router/binance.js
+++ src/router/binance.js
@@ -1,51 +1,54 @@
-const binview = require('binanceview')
-const { removeAndRun, stampMaker, replyer } = require('../utils')
+const WebSocket = require('ws')
+const { removeAndRun, replyer } = require('../utils')
 
-const text = stampMaker({ type: 'md', kind: 'fin', deleteWhen: 'binance' })
+let price = {}
+let treshold = {}
 
-let symbols = [
-  'BTCUSDT',
-  'ETHUSDT',
-  'BNBUSDT',
-  'ADAUSDT',
-  'DOTUSDT',
-  'ATOMUSDT',
-  'BCHUSDT',
-  'XMRUSDT'
-]
+let conn = new WebSocket("wss://stream.binance.com:9443/ws/!miniTicker@arr")
+conn.on('error', function incoming(data) {
+  console.log(data);
+});
 
-const checkAllSignal = async () => {
-  let s = ""
-  let result = await binview.CheckSymbols(symbols)
-  for (let i in symbols) {
-    for (let key in result[i]) {
-      for (let res of result[i][key]) {
-        if (res == '') continue;
-        s += `${symbols[i]} [${key}] ${res}\n`
+const onMessage = (bot) => {
+  conn.on('message', function incoming(data) {
+    let d = JSON.parse(data)
+    for (let symbol of d) {
+      price[symbol.s] = symbol.c
+    }
+    for (let symbol in treshold) {
+      for (let index in treshold[symbol]) {
+        let tres = treshold[symbol][index]
+        if (tres.opr == '<') {
+          if (price[symbol] < tres.prc) {
+            bot.telegram.sendMessage(tres.user, `${symbol} @ ${price[symbol]}`)
+            treshold[symbol].splice(index, 1);
+          }
+        }
+        if (tres.opr == '>') {
+          if (price[symbol] > tres.prc) {
+            bot.telegram.sendMessage(tres.user, `${symbol} @ ${price[symbol]}`)
+            treshold[symbol].splice(index, 1);
+          }
+        }
       }
     }
-  }
-  return s
-}
-
-const sendAllSignal = (ctx) => {
-  const reply = replyer(async () => {
-
-    let s = await checkAllSignal
-
-    if (s == "") return text(`No signal found for all listed symbols`)
-    return text(s);
   })
-  reply(ctx)
 }
 
-const main = (ctx) => {
-  return sendAllSignal(ctx)
+const addSymbol = (ctx) => {
+  let msg = ctx.update.message
+  let cmd = msg.text.split(' ')
+  let sym = cmd[0].toUpperCase()+'USDT'
+  let opr = cmd[1]
+  let prc = parseInt(cmd[2])
+  if (treshold[sym] == undefined) treshold[sym] = []
+  treshold[sym].push({prc, opr, user:msg.chat.id})
+  console.log(treshold)
 }
 
 module.exports = {
   main: (bot) => {
-    bot.command('binance', async (ctx) => removeAndRun(ctx, main))
-  }, 
-  checkAllSignal
+    bot.on('text', async (ctx) => addSymbol(ctx))
+    onMessage(bot);
+  }
 }
