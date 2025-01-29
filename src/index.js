8b84b5172f2977552193713ad27314309fa24a4b
my99wins <my999n@gmail.com>
2025-01-29 10:29:02 +0700

    revisit bot

--- src/index.js
+++ src/index.js
@@ -2,27 +2,32 @@ const Telegraf = require('telegraf')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const useMain = require('./router/main').main
-const useReminder = require('./router/reminder').main
-// const useFood = require('./router/food').main
-// const useAutoFood = require('./router/auto-food').main
-const useBinance = require('./router/binance').main
-// const useAutoBinance = require('./router/auto-binance').main
 
-const bot = new Telegraf(process.env.TOKEN)
+const main = async () => {
+  const useMain = require('./router/main').main
+  // const useReminder = require('./router/reminder').main
+  // const useFood = require('./router/food').main
+  // const useAutoFood = require('./router/auto-food').main
+  const useBinance = require('./router/binance').main
+  // const useAutoBinance = require('./router/auto-binance').main
 
-mongoose
-  .connect(process.env.MONGO, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('connected to mongodb'))
+  const bot = new Telegraf(process.env.TOKEN)
 
-useMain(bot)
-useReminder(bot)
-// useFood(bot)
-// useAutoFood(bot)
-useBinance(bot)
-// useAutoBinance(bot)
+  await mongoose
+    .connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log('connected to mongodb'))
 
-bot.launch()
+  useMain(bot)
+  // useReminder(bot)
+  // useFood(bot)
+  // useAutoFood(bot)
+  useBinance(bot)
+  // useAutoBinance(bot)
+
+  bot.launch()
+}
+
+main()
